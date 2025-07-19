645f4b6e6a87f4809dbabeaa02d98132472f21a5
oneascren <test@test.test>
2025-07-19 14:38:19 +0500

    lesson 3_1

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-from src.schemas import User
+from src.models.models import sample_products
+from src.schemas import Feedback, UserCreate
 
 
 app = FastAPI()
+
+feedbacks = list()
 
 
 @app.get("/")
@@ -16,7 +19,26 @@ def read_custom_message():
     return {"message": "This is a custom message!"}
 
 
-@app.post("/user")
-def is_adult_user(user: User):
-    is_adult = user.age > 17
-    return {"name": user.name, "age": user.age, "is_adult": is_adult}
+@app.post("/create_user")
+def is_adult_user(user: UserCreate):
+    return user
+
+
+@app.post("/feedback")
+def add_feedback(feedback: Feedback, is_premium: bool = False):
+    feedbacks.append(dict(feedback))
+    premium_text = " Ваш отзыв будет рассмотрен в приоритетном порядке." if is_premium else ""
+    return {"message": f"Спасибо, {feedback.name}! Ваш отзыв сохранён.{premium_text}", "total": len(feedbacks)}
+
+
+@app.get("/product/search")
+def get_feedback(keyword: str, category: str = None, limit: int = 10):
+    products = [el for el in sample_products if keyword in el.get("name")]
+    if category:
+        products = [el for el in products if category == el.get("category")]
+    return {"products": products[:limit]}
+
+
+@app.get("/product/{product_id}")
+def get_product(product_id: int):
+    return [el for el in sample_products if el.get("product_id") == product_id][0]
--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,37 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
-class User(BaseModel):
+class UserCreate(BaseModel):
     name: str
-    age: int
+    email: EmailStr
+    age: int | None = Field(ge=0)
+    is_subscribed: bool | None
+
+
+class Contact(BaseModel):
+    email: EmailStr
+    phone: Optional[str] = None
+
+    @field_validator("phone")
+    @classmethod
+    def validate_phone(cls, value: str) -> Optional[str]:
+        if not value:
+            return None
+        if not value.isdigit() or len(value) < 7 or len(value) > 15:
+            raise ValueError("Invalid phone number")
+        return value
+
+
+class Feedback(BaseModel):
+    name: str = Field(min_length=2, max_length=50)
+    message: str = Field(min_length=10, max_length=500)
+    contact: Contact
+
+    @field_validator("message", mode="after")
+    @classmethod
+    def censor(cls, value: str) -> str:
+        if any(word in value for word in ("редис", "бяк", "козявк")):
+            raise ValueError("Value error, Использование недопустимых слов")
+        return value
